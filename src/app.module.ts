9e6962f3fe33d1adadafd6c9dd174bfef1c2ecfb
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-19 19:28:55 +0200

    Link up with app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { getEnvFilePath } from './config/envFilePath';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { DatabaseModule } from './modules/db/database.module';
+import { UserModule } from './modules/user/user.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [getEnvFilePath()],
+    }),
+    DatabaseModule,
+    UserModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
