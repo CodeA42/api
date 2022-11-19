4932816ef5f1bf6e7a71c4c5d978c12ec954793f
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-19 20:13:57 +0200

    Finish basic setup

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  app.use(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new GenericErrorFilter(httpAdapter));
   await app.listen(configService.get<number>(server.port) || 3000);
