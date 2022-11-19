ca0dc03c295cd430c9420ca075dbbae3a4cf556e
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-19 19:19:54 +0200

    add db enums

---
+++ src/config/types.ts
@@ -0,0 +1,8 @@
+export enum database {
+  type = 'DATABASE_TYPE',
+  host = 'DATABASE_HOST',
+  port = 'DATABASE_PORT',
+  username = 'DATABASE_USERNAME',
+  password = 'DATABASE_PASSWORD',
+  name = 'DATABASE_NAME',
+}
