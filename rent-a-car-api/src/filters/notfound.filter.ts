8ded58914b3988093a29ecfe44e7f0106133978d
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-26 16:26:00 +0200

    Added not found filter

---
+++ rent-a-car-api/src/filters/notfound.filter.ts
@@ -0,0 +1,21 @@
+import {
+  ArgumentsHost,
+  Catch,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { UserNotFoundError } from 'src/modules/user/types/user.errors';
+import { VehicleNotFoundError } from 'src/modules/vehicle/types/vehicle.errors';
+import GenericErrorFilter from './generic.filter';
+
+@Catch(UserNotFoundError, VehicleNotFoundError)
+export default class NotFoundErrorFilter extends GenericErrorFilter {
+  catch(err: any, host: ArgumentsHost) {
+    const response = {
+      statusCode: HttpStatus.NOT_FOUND,
+      message: err.message,
+    };
+
+    super.catch(new HttpException(response, response.statusCode), host);
+  }
+}
