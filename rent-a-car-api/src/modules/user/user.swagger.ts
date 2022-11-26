06ef8b8fdcadde4bad3b87185059813281f42f4e
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-26 14:12:18 +0200

    removed uncessary documentation

--- rent-a-car-api/src/modules/user/user.swagger.ts
+++ rent-a-car-api/src/modules/user/user.swagger.ts
@@ -21,32 +21,6 @@ export function ApiUserController() {
   );
 }
 
-export function ApiGetUserChats() {
-  return applyDecorators(
-    ApiOkResponse({
-      description: 'List with the chats the the user is part of',
-      schema: {
-        example: [
-          {
-            id: '76aec10d-b373-4f22-b16c-b5909a0d9e37',
-            name: 'Yaa Yeet',
-            adminId: 'd72d96b9-34aa-456d-8289-cc135b1db8ac',
-          },
-          {
-            id: '5ad7dd4c-99b9-4fe0-860c-f80bf3ba5195',
-            name: 'Yaa Yeet',
-            adminId: 'd72d96b9-34aa-456d-8289-cc135b1db8ac',
-          },
-        ],
-      },
-    }),
-    ApiNotFoundResponse({
-      description: 'Token signature is correct but the user does not exist',
-      schema: { example: { statusCode: 404, message: 'User not found' } },
-    }),
-  );
-}
-
 export function ApiGetUser() {
   return applyDecorators(
     ApiOkResponse({
