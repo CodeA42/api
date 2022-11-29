05fd1971273a5665319663b6a96124fbd795b65b
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-29 08:45:52 +0200

    Update with linter

--- rent-a-car-api/src/modules/vehicle/pipes/vehicle.pipe.ts
+++ rent-a-car-api/src/modules/vehicle/pipes/vehicle.pipe.ts
@@ -1,11 +1,11 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
-import { VehicleOrderBy } from '../types/vehicle.types';
+import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common'
+import { VehicleOrderBy } from '../types/vehicle.types'
 
 @Injectable()
 export class VehicleOrderByPipe implements PipeTransform {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: string, metadata: ArgumentMetadata) {
     // Gets string value from SortByField enum
-    return VehicleOrderBy[value as keyof typeof VehicleOrderBy];
+    return VehicleOrderBy[value as keyof typeof VehicleOrderBy]
   }
 }
