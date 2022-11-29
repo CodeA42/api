53c4a577e852a608f492c7cd58e0dfa79cd7630f
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-29 08:49:05 +0200

    Add missing wiring

--- rent-a-car-api/src/modules/vehicle/entities/vehicle.providers.ts
+++ rent-a-car-api/src/modules/vehicle/entities/vehicle.providers.ts
@@ -2,7 +2,7 @@ import { DatabaseKeys } from 'src/utils/@types/app.types'
 import { DataSource } from 'typeorm'
 import Vehicle from './vehicle.entiity'
 
-export const userRepositoryProvider = {
+export const vehicleRepositoryProvider = {
   provide: DatabaseKeys.VEHICLE_REPOSITORY,
   useFactory: (dataSource: DataSource) => dataSource.getRepository(Vehicle),
   inject: [DatabaseKeys.DATA_SOURCE],
--- rent-a-car-api/src/modules/vehicle/vehicle.module.ts
+++ rent-a-car-api/src/modules/vehicle/vehicle.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { VehicleService } from './vehicle.service';
-import { VehicleController } from './vehicle.controller';
+import { Module } from '@nestjs/common'
+import { VehicleService } from './vehicle.service'
+import { VehicleController } from './vehicle.controller'
+import { vehicleRepositoryProvider } from './entities/vehicle.providers'
+import { VehicleRepository } from './vehicle.repository'
 
 @Module({
-  providers: [VehicleService],
+  providers: [VehicleService, vehicleRepositoryProvider, VehicleRepository],
   controllers: [VehicleController],
+  exports: [VehicleService],
 })
 export class VehicleModule {}
