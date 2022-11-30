a28b8cae3b6188de322718f1463d5e9c40439413
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-30 06:52:05 +0200

    Format with linter

--- rent-a-car-api/src/modules/authentication/authentication.controller.ts
+++ rent-a-car-api/src/modules/authentication/authentication.controller.ts
@@ -8,20 +8,20 @@ import {
   Post,
   Res,
   UseGuards,
-} from '@nestjs/common';
-import { AuthenticationService } from 'src/modules/authentication/authentication.service';
-import { Response } from 'express';
-import { AuthenticationGuard } from 'src/modules/authentication/guards/authentication.guard';
+} from '@nestjs/common'
+import { AuthenticationService } from 'src/modules/authentication/authentication.service'
+import { Response } from 'express'
+import { AuthenticationGuard } from 'src/modules/authentication/guards/authentication.guard'
 import {
   AccessTokenResponseDto,
   LoginAuthDto,
   TokenUserDto,
   UserAuthDto,
-} from './types/authentication.dto';
-import { authType } from './types/authentication.types';
-import { UserIdResponseDto } from 'src/modules/user/types/user.dtos';
-import { Authentication } from './decorators/authentication.decorator';
-import { Cookies } from './decorators/cookies.decorator';
+} from './types/authentication.dto'
+import { authType } from './types/authentication.types'
+import { UserIdResponseDto } from 'src/modules/user/types/user.dtos'
+import { Authentication } from './decorators/authentication.decorator'
+import { Cookies } from './decorators/cookies.decorator'
 import {
   ApiAuthenticationController,
   ApiLogin,
@@ -29,8 +29,8 @@ import {
   ApiLogoutAll,
   ApiRefresh,
   ApiRegister,
-} from './authentication.swagger';
-import { User } from '../user/decorators/user.decorator';
+} from './authentication.swagger'
+import { User } from '../user/decorators/user.decorator'
 
 @Controller('')
 @ApiAuthenticationController()
@@ -40,7 +40,7 @@ export class AuthenticationController {
   @Post('/register')
   @ApiRegister()
   register(@Body() userAuthDto: UserAuthDto): Promise<UserIdResponseDto> {
-    return this.authenticationService.register(userAuthDto);
+    return this.authenticationService.register(userAuthDto)
   }
 
   @ApiLogin()
@@ -53,9 +53,9 @@ export class AuthenticationController {
     const [accessToken, refreshToken] = await this.authenticationService.login(
       loginAuthDto,
       userAgent,
-    );
-    res.cookie('refreshToken', refreshToken, { httpOnly: true });
-    return { accessToken };
+    )
+    res.cookie('refreshToken', refreshToken, { httpOnly: true })
+    return { accessToken }
   }
 
   @ApiRefresh()
@@ -64,7 +64,7 @@ export class AuthenticationController {
   @Authentication(authType.refreshToken)
   @UseGuards(AuthenticationGuard)
   refresh(@User() user: TokenUserDto): AccessTokenResponseDto {
-    return this.authenticationService.refresh(user);
+    return this.authenticationService.refresh(user)
   }
 
   @ApiLogout()
@@ -75,8 +75,8 @@ export class AuthenticationController {
     @Cookies(authType.refreshToken) refreshToken: string,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
-    res.clearCookie(authType.refreshToken);
-    this.authenticationService.logout(refreshToken);
+    res.clearCookie(authType.refreshToken)
+    this.authenticationService.logout(refreshToken)
   }
 
   @Get('logout-all')
@@ -87,7 +87,7 @@ export class AuthenticationController {
     @User('id', ParseUUIDPipe) userId: string,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
-    res.clearCookie(authType.refreshToken);
-    this.authenticationService.logoutAll(userId);
+    res.clearCookie(authType.refreshToken)
+    this.authenticationService.logoutAll(userId)
   }
 }
