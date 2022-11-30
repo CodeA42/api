0d5a72ac6fc8b5a3e3977d134e7e33efac5c44bc
alexander.velichkov <alexander.velichkov@pytek.bg>
2022-11-30 06:52:30 +0200

    Format with linter

--- rent-a-car-api/src/modules/authentication/authentication.service.ts
+++ rent-a-car-api/src/modules/authentication/authentication.service.ts
@@ -1,22 +1,22 @@
-import { Inject, Injectable } from '@nestjs/common';
-import User from 'src/modules/user/entities/user.entity';
+import { Inject, Injectable } from '@nestjs/common'
+import User from 'src/modules/user/entities/user.entity'
 import {
   AccessTokenResponseDto,
   LoginAuthDto,
   TokenUserDto,
   UserAuthDto,
-} from 'src/modules/authentication/types/authentication.dto';
+} from 'src/modules/authentication/types/authentication.dto'
 import {
   EmailExistsError,
   UsernameExistsError,
   WrongCredentialsError,
-} from 'src/modules/authentication/types/authentication.errors';
-import { UserNotFoundError } from 'src/modules/user/types/user.errors';
-import { TokenService } from 'src/modules/authentication/token.service';
-import { BcryptService } from 'src/modules/bcrypt/bcrypt.service';
-import { UserService } from '../user/user.service';
+} from 'src/modules/authentication/types/authentication.errors'
+import { UserNotFoundError } from 'src/modules/user/types/user.errors'
+import { TokenService } from 'src/modules/authentication/token.service'
+import { BcryptService } from 'src/modules/bcrypt/bcrypt.service'
+import { UserService } from '../user/user.service'
 
-Injectable();
+Injectable()
 export class AuthenticationService {
   constructor(
     private readonly tokenService: TokenService,
@@ -27,59 +27,59 @@ export class AuthenticationService {
 
   async register(userAuthDto: UserAuthDto): Promise<{ id: string }> {
     try {
-      await this.userService.findByEmailOrFail(userAuthDto.email);
-      throw new EmailExistsError();
+      await this.userService.findByEmailOrFail(userAuthDto.email)
+      throw new EmailExistsError()
     } catch (e) {
       if (e instanceof UserNotFoundError) {
       } else if (e instanceof EmailExistsError) {
-        throw e;
+        throw e
       } else {
-        throw e;
+        throw e
       }
     }
 
     try {
-      await this.userService.findByUsernameOrFail(userAuthDto.username);
-      throw new UsernameExistsError();
+      await this.userService.findByUsernameOrFail(userAuthDto.username)
+      throw new UsernameExistsError()
     } catch (e) {
       if (e instanceof UserNotFoundError) {
       } else if (e instanceof UsernameExistsError) {
-        throw e;
+        throw e
       } else {
-        throw e;
+        throw e
       }
     }
 
-    const user: User = await this.hashPaswordAndInsertUser(userAuthDto);
-    return { id: user.id };
+    const user: User = await this.hashPaswordAndInsertUser(userAuthDto)
+    return { id: user.id }
   }
 
   async login(
     loginAuthDto: LoginAuthDto,
     userAgent: string,
   ): Promise<[accessToken: string, refreshToken: string]> {
-    let user: User;
+    let user: User
     try {
       if (loginAuthDto.email) {
-        user = await this.userService.findByEmailOrFail(loginAuthDto.email);
+        user = await this.userService.findByEmailOrFail(loginAuthDto.email)
       }
       if (loginAuthDto.username) {
         user = await this.userService.findByUsernameOrFail(
           loginAuthDto.username,
-        );
+        )
       }
     } catch (e) {
       if (e instanceof UserNotFoundError) {
-        throw new WrongCredentialsError();
+        throw new WrongCredentialsError()
       }
-      throw e;
+      throw e
     }
 
     const tokenData: TokenUserDto = {
       id: user.id,
       username: user.username,
       email: user.email,
-    };
+    }
 
     if (
       await this.bcryptService.comparePasswords(
@@ -88,50 +88,50 @@ export class AuthenticationService {
       )
     ) {
       const accessToken: string =
-        this.tokenService.generateAccessToken(tokenData);
+        this.tokenService.generateAccessToken(tokenData)
 
       const refreshToken: string =
-        this.tokenService.generateRefreshToken(tokenData);
+        this.tokenService.generateRefreshToken(tokenData)
 
-      const exp: number = this.tokenService.getTokenExp(refreshToken);
+      const exp: number = this.tokenService.getTokenExp(refreshToken)
       await this.tokenService.insertToken(
         refreshToken,
         exp,
         user.username,
         userAgent,
         user.id,
-      );
+      )
 
-      return [accessToken, refreshToken];
+      return [accessToken, refreshToken]
     }
-    throw new WrongCredentialsError();
+    throw new WrongCredentialsError()
   }
 
   refresh(user: TokenUserDto): AccessTokenResponseDto {
-    const accessTokenDto = new AccessTokenResponseDto();
-    accessTokenDto.accessToken = this.tokenService.generateAccessToken(user);
-    return accessTokenDto;
+    const accessTokenDto = new AccessTokenResponseDto()
+    accessTokenDto.accessToken = this.tokenService.generateAccessToken(user)
+    return accessTokenDto
   }
 
   async logout(token: string): Promise<void> {
-    await this.tokenService.deleteToken(token);
-    return;
+    await this.tokenService.deleteToken(token)
+    return
   }
 
   async hashPaswordAndInsertUser(userAuthDto: UserAuthDto): Promise<User> {
     const hashedPassword = await this.bcryptService.hashPassword(
       userAuthDto.password,
-    );
+    )
 
     return await this.userService.insertUser(
       userAuthDto.username,
       hashedPassword,
       userAuthDto.email,
-    );
+    )
   }
 
   async logoutAll(userId: string) {
-    await this.tokenService.deleteAllTokensFromUser(userId);
-    await this.userService.updateLogoutTime(userId);
+    await this.tokenService.deleteAllTokensFromUser(userId)
+    await this.userService.updateLogoutTime(userId)
   }
 }
